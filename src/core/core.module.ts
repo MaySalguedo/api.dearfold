caafc7c6d384e423f05ae1975e78d7c8849f1266
MaySalguedo <felipemaysalguedo@gmail.com>
2025-09-08 16:30:29 -0500

    Appwrite injection

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NotFoundException } from '@nestjs/common';
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/env.config';
@@ -16,6 +16,8 @@ import { JwtGuard } from './guards/jwt/jwt.guard';
 import { AdminGuard } from './guards/admin/admin.guard';
 import { TokenGuard } from './guards/token/token.guard';
 
+import { Client, Storage } from 'node-appwrite';
+
 @Module({
 
 	imports: [
@@ -29,7 +31,35 @@ import { TokenGuard } from './guards/token/token.guard';
 
 	], providers: [
 
-		AuthStrategy, JwtStrategy, AuthGuard, JwtGuard, AdminGuard, TokenStrategy, TokenGuard
+		AuthStrategy, JwtStrategy, AuthGuard, JwtGuard, AdminGuard, TokenStrategy, TokenGuard, {
+
+			provide: Client,
+			useFactory: () => {
+
+				const endpoint: string | undefined = process.env.APPWRITE_ENDPOINT;
+				const project: string | undefined = process.env.APPWRITE_PROJECT_ID;
+				const key: string | undefined = process.env.APPWRITE_KEY_SECRET;
+
+				if (!endpoint || !project || !key){
+
+					throw new NotFoundException('Appwrite environment variables could not be found.');
+
+				}
+
+				return new Client().setEndpoint(endpoint).setProject(project).setKey(key);
+
+			}
+
+		}, {
+
+			provide: Storage,
+			useFactory: (client: Client) => {
+
+				return new Storage(client);
+
+			}
+
+		}
 
 	], exports: []
 
